3920ea45a2e5c6e7584ae623808768170455413d
JunHyeock Park <wnsgur6311@gmail.com>
2019-09-14 17:51:21 +0900

    Fetch datas && render

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import { movieApiRequests } from "./api"
 
 function App() {
-  return <h1>hi</h1>
+  const [movieDatas, setMovieDatas] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const fetchMoviDatas = async () => {
+    try {
+      const {
+        data: {
+          data: { movies: movieDatas }
+        }
+      } = await movieApiRequests.getMovies()
+      setMovieDatas(movieDatas)
+      console.log(movieDatas)
+    } catch {
+      setError("Error: Can't get movie datas")
+    } finally {
+      setLoading(false)
+    }
+  }
+  useEffect(() => {
+    fetchMoviDatas()
+  }, [])
+  return error ? (
+    <span>{error}</span>
+  ) : loading ? (
+    <span>Loading...</span>
+  ) : (
+    <div>
+      {movieDatas.map(movie => (
+        <div key={movie.id}>{movie.title}</div>
+      ))}
+    </div>
+  )
 }
 
 export default App
---
+++ src/api.js
@@ -0,0 +1,9 @@
+import axios from "axios"
+
+const movieApi = axios.create({
+  baseURL: "https://yts.lt/api/v2"
+})
+
+export const movieApiRequests = {
+  getMovies: (page = 1) => movieApi.get("/list_movies.json", { params: { page, limit: 50 } })
+}
